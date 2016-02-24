b5f27de4f9b9888649817e9ab6cf5f4715102786
AndyH <caim0725@gmail.com>
2016-02-24 10:36:12 +0800

    More clever way.

--- title_case_codewars.py
+++ title_case_codewars.py
@@ -1,16 +1,10 @@
 def title_case(title, minor_words=''):
     '''type in title and return all in capitalize 
     if word not in minor_words'''
-    if title == '':return ''
-    split_title = title.lower().split()
-    minor_list = minor_words.lower().split()
-    new_title = ''
-    for word in split_title:
-        if word not in minor_list:
-            new_title += word[0].upper() + word[1:] +' '
-        else:new_title += word + ' '
-    new_title = (new_title[0].upper() + new_title[1:]).rstrip()
-    return new_title
+    title = title.capitalize().split()
+    minor_words = minor_words.lower().split()
+    return ' '.join([word if word in minor_words else word.capitalize() 
+                    for word in title])
 
 print title_case('')
 print title_case('THE WIND IN THE WILLOWS', 'The In')
